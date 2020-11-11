dda68d13b06191f43c64e38cc44b7f12670a47c0
andresecheverrijaramillo <68928458+andresecheverrijaramillo@users.noreply.github.com>
2020-11-11 02:22:12 -0500

    Add files via upload

---
+++ laboratorios/lab05/ejercicioEnLinea/Algorithm.java
@@ -0,0 +1,10 @@
+public class Algorithm {
+    public static boolean DFSColorFC(Tree tree) {
+        return DFSColorFCAux(tree.getFirst(), 1);
+    }
+
+    private static boolean DFSColorFCAux(Node first, int color) {
+
+       return true;
+    }
+}
